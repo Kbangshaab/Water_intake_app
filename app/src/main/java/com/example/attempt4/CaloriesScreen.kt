835663fc4b76f7d1ce8d732c494f2c2e607cff0b
Kbangshaab <73216803+Kbangshaab@users.noreply.github.com>
2024-06-09 23:42:44 +0200

    Update CaloriesScreen.kt

--- app/src/main/java/com/example/attempt4/CaloriesScreen.kt
+++ app/src/main/java/com/example/attempt4/CaloriesScreen.kt
@@ -8,7 +8,8 @@ import androidx.navigation.NavController
 
 @Composable
 fun CaloriesScreen(navController: NavController){
-    val url = "https://www.calculator.net/calorie-calculator.html"
+    val url = "https://example.com"
+    //Could be used the same way as water intake with a database, but we used a website
     AndroidView(factory = {
         WebView(it).apply {
             webViewClient = WebViewClient()
